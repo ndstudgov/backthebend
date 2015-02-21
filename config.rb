70c5e1984444ef2b7c50b61af2ebc3039edc6b33
Shuyang Li <shuyang.li.95@gmail.com>
2015-02-20 20:35:13 -0500

    Initial styling for card page

--- config.rb
+++ config.rb
@@ -36,9 +36,12 @@
 # activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
+configure :development do
+  activate :livereload
+end
+
+# Directory index
+activate :directory_indexes
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -48,18 +51,16 @@
 # end
 
 set :css_dir, 'stylesheets'
-
 set :js_dir, 'javascripts'
-
 set :images_dir, 'images'
 
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
   # activate :asset_hash
